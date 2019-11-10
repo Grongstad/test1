e72b798d7d40e569714c732f1e21961b495f4927
hgrongstad <hgrongstad@gmail.com>
2019-11-10 17:26:18 -0500

    First commit

---
+++ src/test/java/com/grongstad/spring/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grongstad.spring.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
